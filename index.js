0aa26daa91436b8ebf81ca2903335dc15ebf03cf
dvicampos <dvicamp@gmail.com>
2024-03-02 20:22:26 -0600

    render con nodejs

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const mongoURI = 'mongodb+srv://davecamp:R8dy0LYFUlCh54lp@cluster0.fdwpzyb.mongodb.net/miapp?retryWrites=true&w=majority';
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Conexión a MongoDB Atlas exitosa'))
+.catch(err => console.log(err));
+
+const contactoSchema = new mongoose.Schema({
+    nombre: String,
+    correo: String,
+    numero: Number,
+    mensaje: String,
+    nombreempresa: String
+});
+const contacto = mongoose.model('contacto', contactoSchema);
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/crear', async (req, res) => {
+    try {
+      const { nombre, correo, numero, mensaje, nombreempresa } = req.body;
+      const nuevocontacto = new contacto({ nombre, correo, numero, mensaje, nombreempresa });
+      await nuevocontacto.save();
+      console.log("guardado")
+      setTimeout(() => {
+        res.redirect('/');
+      }, 3000);
+    } catch (err) {
+      console.error('Error al crear un nuevo contacto:', err);
+      res.status(500).send('Error interno del servidor');
+    }
+  });
+
+app.listen(port, () => {
+    console.log(`servidor en: ${port}`);
+});
---
+++ public/js/efectos.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+
+// Scroll Elementos Menu
+	var acercaDe = $('#acerca-de').offset().top,
+		planes = $('#planes').offset().top,
+		galeria = $('#galeria').offset().top,
+		contacto = $('#contacto').offset().top;
+
+// Boton Acerca de
+	$('#btn-acerca-de').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: 380
+		}, 500);
+	});
+
+	$('#btn-planes').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: planes - 40
+		});
+	});
+
+	$('#btn-galeria').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: galeria - 40
+		});
+	});
+
+	$('#btn-contacto').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: contacto - 40
+		});
+	});
+
+// Efecto Menu
+	$('.planes a').each(function(index, elemento){
+		$(this).css({
+			'top': '-200px'
+		});
+
+		$(this).animate({
+			top: '0'
+		},2000 + (index * 500));
+	});
+
+
+// Efecto Header
+	if ($(window).width() > 800){
+		$('header .textos').css({
+			opacity: 0,
+			marginTop: 0
+		});
+
+		$('header .textos').animate({
+			opacity: 1,
+			marginTop: '-52px'
+		},1500);
+	}
+
+});
+
+
+function showModal(message) {
+	const modal = document.getElementById('myModal');
+	const modalMessage = document.getElementById('modalMessage');
+	modalMessage.textContent = message;
+	modal.style.display = 'block';
+  }
+  
+  document.getElementById('openModalBtn').onclick = function() {
+	showModal('Contacto creado exitosamente');
+  };
+  
+  document.getElementsByClassName('close')[0].onclick = function() {
+	const modal = document.getElementById('myModal');
+	modal.style.display = 'none';
+  };
+  
+  window.onclick = function(event) {
+	const modal = document.getElementById('myModal');
+	if (event.target === modal) {
+	  modal.style.display = 'none';
+	}
+  };
+  
