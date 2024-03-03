61ec1dd893776c6bb8940ba76df1ea092dede6d3
dvicampos <dvicamp@gmail.com>
2024-03-03 01:00:44 -0600

    redes sociales y botono inicio

--- public/js/efectos.js
+++ public/js/efectos.js
@@ -126,3 +126,18 @@ function showModal(message) {
 	}
   };
   
+
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.querySelector('.scroll-to-top').style.display = 'block';
+  } else {
+    document.querySelector('.scroll-to-top').style.display = 'none';
+  }
+}
+
+document.querySelector('.scroll-to-top').addEventListener('click', function() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
