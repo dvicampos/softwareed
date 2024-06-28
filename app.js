2ae663ff3f4aa2f4a98c84f8cf9aad8ae6bd8136
dvicampos <dvicamp@gmail.com>
2024-06-28 13:07:45 -0600

    name file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const mongoURI = 'mongodb+srv://davecamp:R8dy0LYFUlCh54lp@cluster0.fdwpzyb.mongodb.net/miapp?retryWrites=true&w=majority';
 mongoose.connect(mongoURI, {
